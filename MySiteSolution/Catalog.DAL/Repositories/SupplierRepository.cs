4187d5909a32d7b37440c55db470332980b332e6
Alex Puchok <a.puchek.90@gmail.com>
2022-10-16 17:47:56 +0300

    clean imports

--- MySiteSolution/Catalog.DAL/Repositories/SupplierRepository.cs
+++ MySiteSolution/Catalog.DAL/Repositories/SupplierRepository.cs
@@ -1,8 +1,5 @@
-﻿using Catalog.DAL.Entities;
-using Catalog.DAL.DataContext;
-using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Catalog.DAL.DataContext;
+using Catalog.DAL.Entities;
 
 namespace Catalog.DAL.Repositories
 {
@@ -14,31 +11,5 @@ namespace Catalog.DAL.Repositories
         {
             catalogDBContext = context;
         }
-/*
-        public override Category GetById(int id)
-        {
-            var result = catalogDBContext.Categories.AsNoTracking().Include(x => x.Products).FirstOrDefault(x => x.CategoryId == id);
-            return result;
-        }
-
-        public override IList<Category> GetAll()
-        {
-            var result = catalogDBContext.Categories.AsNoTracking().Include(x => x.Products).ToList();
-            return result;
-        }
-
-        public void Delete(int id)
-        {
-            var category = new Category() { CategoryId = id };
-            var products = catalogDBContext.Products.Where(_=>_.CategoryId == id);
-            foreach (var product in products)
-            {
-                catalogDBContext.Products.Attach(product);
-                catalogDBContext.Products.Remove(product);
-            }
-            catalogDBContext.Categories.Attach(category);
-            catalogDBContext.Categories.Remove(category);
-            catalogDBContext.SaveChanges();
-        }*/
     }
 }
