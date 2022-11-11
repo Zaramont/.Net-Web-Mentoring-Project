926504b42dd6ff71e17be8432ac96bca782eab48
Alex Puchok <a.puchek.90@gmail.com>
2022-11-11 21:24:36 +0300

    add task 1 and 2

---
+++ MySiteSolution/MySite/Components/BreadCrumbs.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCatalogSite.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MyCatalogSite.Components
+{
+    public class BreadCrumbs : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            string controller = RouteData.Values["controller"].ToString();
+            string action = RouteData.Values["action"].ToString();
+
+            var list = new List<BreadCrumbPart>();
+
+            if (controller.Equals("Home")) return View(list);
+            list.Add(new BreadCrumbPart { Text = "Home", RelativePath = "/" });
+
+            if (action.Equals("Index"))
+            {
+                list.Add(new BreadCrumbPart { Text = controller, RelativePath = $"/{controller}", IsLastPart = true });
+            }
+            else
+            {
+                list.Add(new BreadCrumbPart { Text = controller, RelativePath = $"/{controller}" });
+                list.Add(new BreadCrumbPart { Text = action, IsLastPart = true });
+            }
+
+            return View(list);
+        }
+    }
+}
---
+++ MySiteSolution/MySite/Models/BreadCrumbPart.cs
@@ -0,0 +1,9 @@
+﻿namespace MyCatalogSite.Models
+{
+    public class BreadCrumbPart
+    {
+        public string Text { get; set; }
+        public string RelativePath  { get; set; }
+        public bool IsLastPart { get; set; }
+    }
+}
