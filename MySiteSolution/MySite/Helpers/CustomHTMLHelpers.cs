a373a93530c59561b515a435301fbe8f85f794e4
Alex Puchok <a.puchek.90@gmail.com>
2022-11-11 22:41:24 +0300

    add tag and html helpers for generating image links

---
+++ MySiteSolution/MySite/Helpers/CustomHTMLHelpers.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+
+namespace MyCatalogSite.Helpers
+{
+    public static class CustomHTMLHelpers
+    {
+        public static IHtmlContent NorthwindImageLink(this IHtmlHelper htmlHelper, int id, string linkName)
+        {
+            return new HtmlString($"<a href=\"/images/{id}\">{linkName}</a>");
+        }
+
+        /*public static String HelloWorldString(this IHtmlHelper htmlHelper)
+            => "<strong>Hello World</strong>";*/
+
+    }
+}
